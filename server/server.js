b84a38202e0ce4da4fa8372b260badccff840ada
Parth Malavia <69411753+ParthMalavia@users.noreply.github.com>
2021-05-28 10:53:46 +0530

    initial commit

---
+++ server/server.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+const Document = require("./Document");
+
+mongoose.connect("mongodb://localhost/google-docs-clone", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+});
+
+const io = require("socket.io")(3001, {
+  cors: {
+    origin: "http://localhost:3000",
+    // origin: "http://192.168.1.12:3000",//to use on diff. devices
+    methods: ["GET", "POST"],
+  },
+});
+
+const defaultValue = "";
+
+io.on("connection", (socket) => {
+  socket.on("get-document", async (documentId) => {
+    const document = await findOrCreateDocument(documentId);
+
+    socket.join(documentId);
+    socket.emit("load-document", document.data);
+
+    socket.on("send-changes", (delta) => {
+      socket.broadcast.to(documentId).emit("receive-changes", delta);
+    });
+
+    socket.on("save-document", async (data) => {
+      await Document.findByIdAndUpdate(documentId, { data });
+    });
+  });
+});
+
+async function findOrCreateDocument(id) {
+  if (id == null) return;
+
+  const document = await Document.findById(id);
+  if (document) return document;
+  return await Document.create({ _id: id, data: defaultValue });
+}
